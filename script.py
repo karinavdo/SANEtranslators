7b54ddf925ceb17157114b57dc2b3636b5ba29fa
Joris van Zundert <joris.van.zundert@gmail.com>
2023-07-05 13:40:37 +0200

    refactor: adapting to kb prerequisites

--- script.py
+++ script.py
@@ -1,20 +1,37 @@
 import os
+import sys
 from glob import glob
 import re
 import csv
 import collections
+import shutil
 
 INPUT_PATH = './input_test'
 OUTPUT_PATH = './output_test'
 
-environ = os.getenv( 'PYENV' )
+paths = { 
+   'input_path': INPUT_PATH,
+   'output_path': OUTPUT_PATH
+   }
 
-if environ is not None and environ=='production':
-    INPUT_PATH = '/input'
-    OUTPUT_PATH = '/output'
+def set_paths( cmd_arg, paths ):
+    if( len( cmd_arg ) == 2 ):
+       if( cmd_arg[0] == '-i' ):
+        paths['input_path'] = cmd_arg[1]
+       if( cmd_arg[0] == '-o' ):
+        paths['output_path'] = cmd_arg[1]
 
-filepaths = glob( f'{INPUT_PATH}/*.txt' )
+# As per spec, so I'm assuming no need for argparse
+# or other over engineered thingies. 
+args = sys.argv[1:]
+for arg1, arg2 in zip( args[::2], args[1::2]):
+   set_paths( (arg1, arg2), paths)
 
+isbns_filepath = f'{paths["input_path"]}/isbns.csv'
+if( os.path.isfile( isbns_filepath ) ):
+    shutil.copyfile( isbns_filepath, f'{paths["output_path"]}/isbns.csv' )
+        
+filepaths = glob( f'{paths["input_path"]}/*.txt' )
 
 frequency_lists={}
 for filepath in filepaths:
@@ -29,7 +46,7 @@ for filepath in filepaths:
 for file_name, frequencies in frequency_lists.items():
     file_name = file_name.split( '/' )[-1]
     file_name = file_name.replace( '.txt', '.csv' )
-    file_path = f'{OUTPUT_PATH}/{file_name}'
+    file_path = f'{paths["output_path"]}/{file_name}'
     with open( file_path, 'w', newline='', encoding='utf8' ) as csv_file:
         writer=csv.writer( csv_file, delimiter=',', quoting=csv.QUOTE_NONNUMERIC )
         writer.writerows( frequencies )
